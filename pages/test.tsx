854be562a48fe5b32b1e350ddebf5e8fb4e1a2f9
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-24 00:36:44 +0900

    feat: 임시 테스트 페이지 UI 업데이트

--- pages/test.tsx
+++ pages/test.tsx
@@ -40,22 +40,41 @@ const Test: NextPage = () => {
   };
 
   return (
-    <div>
-      <h1>API 테스트 페이지 - api.github.com</h1>
-      <input type="text" placeholder="example - /user" ref={inputRef} />
-      <button onClick={test}>API 테스트</button>
-      <button onClick={logout}>로그아웃</button>
+    <section>
       <style jsx={true}>
         {`
+          div {
+            width: fit-content;
+            padding: 1rem;
+            display: flex;
+            flex-direction: column;
+            gap: 0.1rem;
+          }
+          h1 {
+            font-size: 1.66rem;
+            white-space: nowrap;
+            margin-bottom: 1rem;
+          }
+          input {
+            margin: 0.33rem 0;
+            padding: 0.33rem;
+            font-size: 1.1rem;
+          }
           pre {
             width: fit-content;
             margin: 1rem;
           }
         `}
       </style>
+      <div>
+        <h1>[ API 테스트 페이지 - api.github.com ]</h1>
+        <input type="text" placeholder="example - /user" ref={inputRef} />
+        <button onClick={test}>API 테스트</button>
+        <button onClick={logout}>로그아웃</button>
+      </div>
       {res.error && <pre style={{ color: 'red' }}>{res.error}</pre>}
       {res.data && <pre>{res.data}</pre>}
-    </div>
+    </section>
   );
 };
 
