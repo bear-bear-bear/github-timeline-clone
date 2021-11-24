ffa2bb952faac69b7d886a1d3f96c8a5edeabea5
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-25 06:47:41 +0900

    fix: 검색창 클릭 시 액션 발생 안하는 문제 수정

--- components/service/Header/Parts/SearchInput/SearchList/styles.tsx
+++ components/service/Header/Parts/SearchInput/SearchList/styles.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 export const SearchList = styled.ul`
   width: 100%;
   position: absolute;
+  z-index: 999;
   background-color: ${({ theme }) => theme.color.serviceMainBgColor};
   border: 1px solid ${({ theme }) => theme.color.serviceHeaderInputBorderColor}; ;
 `;
