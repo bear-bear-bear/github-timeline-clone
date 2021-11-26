b9b6d08e5d41f02b62c3930c317afae1bd73bbd6
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-27 01:21:56 +0900

    feat: notification store root store에 적용

--- stores/index.tsx
+++ stores/index.tsx
@@ -2,14 +2,17 @@ import { createContext } from 'react';
 import type { FC } from 'react';
 import MyRepositoryStore from './MyRepository';
 import StarredOrWatchedRepositoryStore from './StarredOrWatchedRepository';
+import NotificationStateStore from './NotificationState';
 
 export class RootStore {
   myRepository;
   starredOrWatchedRepository;
+  notificationState;
 
   constructor() {
     this.myRepository = new MyRepositoryStore(this);
     this.starredOrWatchedRepository = new StarredOrWatchedRepositoryStore(this);
+    this.notificationState = new NotificationStateStore(this);
   }
 }
 export const rootStore = new RootStore();
