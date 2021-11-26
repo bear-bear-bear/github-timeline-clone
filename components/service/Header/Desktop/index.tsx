6fb5183144fb7206e0109efaf5acac56836b37d8
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-27 01:22:45 +0900

    feat: 데스크탑 헤더에 알림벨 임의 추가

--- components/service/Header/Desktop/index.tsx
+++ components/service/Header/Desktop/index.tsx
@@ -3,6 +3,7 @@ import GithubIcon from '@components/common/GithubIcon';
 import { useTheme } from '@emotion/react';
 import HeaderWrapper from '../parts/common/HeaderWrapper';
 import SearchInput from '../parts/common/SearchInput';
+import NotificationBell from '../parts/common/NotificationBell';
 import Navigation from '../parts/desktop/Navigation';
 import * as S from './styles';
 import MoreButton from '@components/service/Header/parts/desktop/MoreButton';
@@ -17,6 +18,7 @@ const DesktopHeader = observer(() => {
         <SearchInput />
         <Navigation />
         <MoreButton />
+        <NotificationBell />
       </S.DesktopHeader>
     </HeaderWrapper>
   );
