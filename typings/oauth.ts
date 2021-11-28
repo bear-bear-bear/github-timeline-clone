e32a8696726632f4ab9eed6f48582653492c16c8
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-29 05:43:42 +0900

    feat: 응답엔 있지만 타입 모듈엔 누락되어 있는 타입 임의 정의

--- typings/oauth.ts
+++ typings/oauth.ts
@@ -1,4 +1,22 @@
+import '@octokit/types';
 import type { Endpoints } from '@octokit/types';
+import { CustomEvent } from '@octokit/types';
+
+declare module '@octokit/types' {
+  export type CustomEvent = {
+    payload: {
+      // 응답엔 있지만 타입 선언엔 누락된 타입들 임시 보완
+      pull_request?: {
+        title: string;
+        html_url: string;
+      };
+      review?: {
+        title: string;
+        html_url: string;
+      };
+    };
+  };
+}
 
 export type FetchState = 'init' | 'loading' | 'done' | 'error';
 
@@ -23,6 +41,6 @@ export type Notification = Unpacked<Notifications>;
 
 export type Events =
   Endpoints['GET /users/{username}/events']['response']['data'];
-export type Event = Unpacked<Events>;
+export type Event = Unpacked<Events> & CustomEvent;
 
 export type User = Endpoints['GET /user']['response']['data'];
