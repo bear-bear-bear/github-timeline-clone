ce6181f951ec67ae5035acf7c9e1d859a3aab072
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-30 23:48:33 +0900

    feat: 푸터 작성 및 스타일링

--- components/service/Main/Footer/index.tsx
+++ components/service/Main/Footer/index.tsx
@@ -1,8 +1,28 @@
 import { observer } from 'mobx-react-lite';
+import { github } from '@lib/oauth';
+import navItems from './navItems';
 import * as S from './styles';
 
-const Footer = observer(() => {
-  return <S.Footer>아래</S.Footer>;
-});
+const Footer = observer(() => (
+  <S.Footer>
+    <S.LeftSection>
+      <a title="Home page" href={github.HOST}>
+        <S.GithubIcon size="24px" />
+        <span>© 2021 GitHub, Inc.</span>
+      </a>
+    </S.LeftSection>
+    <S.RightSection>
+      {navItems.map((column) => (
+        <ul key={column.reduce((acc, v) => acc + v.url, '')}>
+          {column.map((navItem) => (
+            <li key={navItem.url}>
+              <a href={navItem.url}>{navItem.label}</a>
+            </li>
+          ))}
+        </ul>
+      ))}
+    </S.RightSection>
+  </S.Footer>
+));
 
 export default Footer;
