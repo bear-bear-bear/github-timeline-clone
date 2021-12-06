6f4d94ce675b37b601c33143721da0aaae3983fd
bearbearbear <ghlwkdthfl@gmail.com>
2021-12-07 03:13:35 +0900

    feat: 쿠키를 persistant하게 변경 (7일)

--- lib/session.ts
+++ lib/session.ts
@@ -9,6 +9,12 @@ export type AuthInfo = {
 export const sessionOptions: IronSessionOptions = {
   password: process.env.COOKIE_PASSWORD as string,
   cookieName: 'accessToken/sid',
+  cookieOptions: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    path: '/',
+    maxAge: 7 * 24 * 60 * 60,
+  },
 };
 
 // This is where we specify the typings of req.session.*
